66dba32f7fbb6f8b9cd37608db4e10ec66744e12
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-24 23:01:27 +0530

    working version with ng2-nvd3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { DataService } from './data.service';
+// import * as d3 from 'd3';
+
+
+declare let d3: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+    '../../node_modules/nvd3/build/nv.d3.css'
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private dataService: DataService){}
+  @ViewChild('chart') private chartContainer: ElementRef;
 
-  onData(){
+  options;
+  data;
+  values = [];
+  recData: Array<any>;
+  private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
+  private width: number = 1500;
+  private height: number = 700;
+
+
+  constructor(private dataService: DataService) {
     this.dataService.getData().subscribe(
       (res) => {
-        console.log(res.json());
+        this.recData = res.json()["data"];
+        // console.log(this.data);
+        for (var i = 0; i < this.recData.length; i++) {
+          let obj = { "label": this.recData[i][0], "value": this.recData[i][1] };
+          this.values.push(obj);
+        }
       }
     );
+  }
+
+  onData() {
+    this.createChart();
+  }
+
+  ngOnInit() {
+    // this.createChart();
+  }
+
+  createChart() {
+
+    this.options = {
+      chart: {
+        type: 'discreteBarChart',
+        height: this.height,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 50,
+          left: 80
+        },
+        x: function (d) { return d.label; },
+        y: function (d) { return d.value; },
+
+        duration: 500,
+        xAxis: {
+          axisLabel: 'Year'
+        },
+        yAxis: {
+          axisLabel: 'GDP',
+          axisLabelDistance: -10
+        }
+      }
+    }
+
+    this.data = [
+      {
+        key: "GDP",
+        values: this.values
+      }
+    ];
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
+import { NvD3Component } from 'ng2-nvd3';
+
+import 'd3';
+import 'nvd3';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvD3Component
   ],
   imports: [
     BrowserModule,
