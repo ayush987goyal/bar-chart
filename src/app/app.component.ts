f3e808889ebae7b99572fe275803d32f645b64b8
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-26 22:14:40 +0530

    Added primeng chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,11 @@ export class AppComponent implements OnInit {
 
   options;
   data;
+  dataP; any;
   values = [];
   recData: Array<any>;
+  xData = [];
+  yData = [];
   private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
   private width: number = 1500;
   private height: number = 700;
@@ -33,6 +36,20 @@ export class AppComponent implements OnInit {
         for (var i = 0; i < this.recData.length; i++) {
           let obj = { "label": this.recData[i][0], "value": this.recData[i][1] };
           this.values.push(obj);
+          this.xData.push(this.recData[i][0]);
+          this.yData.push(this.recData[i][1]);
+        }
+
+        this.dataP = {
+          labels: this.xData,
+          datasets: [
+            {
+              label: 'GDP',
+              backgroundColor: '#42A5F5',
+              borderColor: '#1E88E5',
+              data: this.yData
+            }
+          ]
         }
       }
     );
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
-import { NvD3Component } from 'ng2-nvd3';
+// import { NvD3Component } from 'ng2-nvd3';
+import {ChartModule} from 'primeng/primeng';
 
 import 'd3';
 import 'nvd3';
@@ -11,10 +13,12 @@ import 'nvd3';
 @NgModule({
   declarations: [
     AppComponent,
-    NvD3Component
+    // NvD3Component
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ChartModule,
     HttpModule
   ],
   providers: [DataService],
